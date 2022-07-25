d2508aa0acd939a0481cad199a37e0a482f0fb52
Sebastian Colombo <scolombo30@studenti.uninsubria.it>
2022-07-25 11:00:25 +0200

    Add gui and adapt script to new gui

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+let playerChoice;
+let computerChoice;
 let playerWins = 0;
 let computerWins = 0;
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissor');
+
+const buttons = [rock, paper, scissors];
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playerChoice = button.id;
+        computerChoice = getComputerChoice();
+        singleRound(playerChoice,computerChoice);
+        updateScore();
+        }
+      )
+    }
+)
 
 function getComputerChoice(){
     var randomNumber = Math.floor(Math.random() * 3);
@@ -15,30 +34,34 @@ function getComputerChoice(){
 
 function singleRound(playerChoice, computerChoice) {
     playerChoice=playerChoice.toLowerCase();
-    if (playerChoice === computerChoice) {  return "It's a tie!"; }  // if player and computer choose the same thing, it's a tie
-    if (playerChoice === 'rock' && computerChoice === 'paper') {computerWins++; return "You lose! Paper beats rock.";} // if player chooses rock and computer chooses paper, computer wins
-    if (playerChoice === 'rock' && computerChoice === 'scissors') {playerWins++; return "You win! Rock beats scissors.";} // if player chooses rock and computer chooses scissors, player wins
-    if (playerChoice === 'paper' && computerChoice === 'rock') {playerWins++; return "You win! Paper beats rock.";} // if player chooses paper and computer chooses rock, player wins
-    if (playerChoice === 'paper' && computerChoice === 'scissors') {computerWins++; return "You lose! Scissors beats paper.";} // if player chooses paper and computer chooses scissors, computer wins
-    if (playerChoice === 'scissors' && computerChoice === 'rock')  {computerWins++; return "You lose! Rock beats scissors.";} // if player chooses scissors and computer chooses rock, computer wins
-    if (playerChoice === 'scissors' && computerChoice === 'paper') {playerWins++; return "You win! Scissors beats paper.";} // if player chooses scissors and computer chooses paper, player wins
+    if (playerChoice === computerChoice) {  return }  // if player and computer choose the same thing, it's a tie
+    if (playerChoice === 'rock' && computerChoice === 'paper') {computerWins++; } // if player chooses rock and computer chooses paper, computer wins
+    if (playerChoice === 'rock' && computerChoice === 'scissors') {playerWins++; } // if player chooses rock and computer chooses scissors, player wins
+    if (playerChoice === 'paper' && computerChoice === 'rock') {playerWins++; } // if player chooses paper and computer chooses rock, player wins
+    if (playerChoice === 'paper' && computerChoice === 'scissors') {computerWins++; } // if player chooses paper and computer chooses scissors, computer wins
+    if (playerChoice === 'scissors' && computerChoice === 'rock')  {computerWins++;} // if player chooses scissors and computer chooses rock, computer wins
+    if (playerChoice === 'scissors' && computerChoice === 'paper') {playerWins++;} // if player chooses scissors and computer chooses paper, player wins
 }
 
-function game(){
-    var result;
-    var playerChoice;
-    for (let i = 0; i < 5; i++) {
-        playerChoice = prompt("Rock, paper, or scissors?");
-        result=singleRound(playerChoice, getComputerChoice());
-        console.log(result);
-        console.log(`Player: ${playerWins} Computer: ${computerWins}`);
+function updateScore(){
+    const player_choice_span = document.getElementById('player_choice');
+    const computer_choice_span = document.getElementById('computer_choice');
+    const span_playerWins = document.getElementById('player_score');
+    const span_computerWins = document.getElementById('computer_score');
+    const score = document.getElementById('score');
+    player_choice_span.textContent = playerChoice;
+    computer_choice_span.textContent = computerChoice;
+    if (playerWins === 5) {
+       score.textContent = 'You win!';
+        return;
     }
-        
-    if (playerWins>computerWins) {
-        alert("You win!");
-    }else{
-        alert("You lose!");
+    if (computerWins === 5) {
+        score.textContent = 'You lose!';
+        return;
     }
+    if (playerWins === 5 || computerWins === 5) {
+        return;
+    }
+    span_playerWins.textContent = playerWins;
+    span_computerWins.textContent = computerWins;
 }
-
-game();
